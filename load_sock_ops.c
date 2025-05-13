8fdf25286fe0664260d400a90006666c4329ea70
root <sofiane.alhoz@gmail.com>
2025-05-13 20:45:49 +0000

    moving initmap from load to changecc, for debian vm

--- load_sock_ops.c
+++ load_sock_ops.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <linux/bpf.h>
-//#include <bpf/bpf.h>
-//#include "bpf_load.h"
 #include <bpf/libbpf.h>
 #include <bpf/bpf.h>
 #include <unistd.h>
@@ -16,134 +14,178 @@
 #include <sys/sem.h>
 #include <sys/shm.h>
 #include <sys/types.h>
+#include <arpa/inet.h> 
 
-#define DEBUGFS "/sys/kernel/debug/tracing"
-////
-////
-////
-////
+#define DEBUGFS "/sys/kernel/debug/tracing/"
+
+struct connection_tuple {
+    __u32 src_ip;
+    __u32 dst_ip;
+    __u16 src_port;
+    __u16 dst_port;
+};
+
+void init_map(int map_fd)
+{
+    __u32 common_ip = 0x7F000001;  // 127.0.0.1
+    __u16 common_src_port = 5001;
+    
+    struct {
+        struct connection_tuple key;
+        char value[16];
+    } entries[] = {
+        {{ common_ip, common_ip, common_src_port, 0 }, "reno"},
+        {{ common_ip, common_ip, common_src_port, 5004 }, "cubic"},
+        {{ common_ip, common_ip, common_src_port, 5005 }, "illinois"},
+        {{ common_ip, common_ip, common_src_port, 5006 }, "vegas"},
+        {{ common_ip, common_ip, common_src_port, 5007 }, "bbr"},
+        {{ common_ip, common_ip, common_src_port, 5008 }, "westwood"},
+        {{ common_ip, common_ip, common_src_port, 5009 }, "highspeed"},
+        {{ common_ip, common_ip, common_src_port, 5010 }, "hybla"},
+        {{ common_ip, common_ip, common_src_port, 5011 }, "scalable"},
+        {{ common_ip, common_ip, common_src_port, 5012 }, "yeah"},
+        {{ common_ip, common_ip, common_src_port, 5013 }, "dctcp"},
+    };
+    
+    int i;
+    for (i = 0; i < sizeof(entries)/sizeof(entries[0]); i++) {
+        if (bpf_map_update_elem(map_fd, &entries[i].key, entries[i].value, BPF_ANY)) {
+            fprintf(stderr, "Failed to update map for %s: %s\n", 
+                    entries[i].value, strerror(errno));
+        }
+    }
+    
+    printf("Map initialized with %d entries\n", i);
+}
 
 void read_trace_pipe(void)
 {
     int trace_fd;
 
-	trace_fd = open(DEBUGFS "trace_pipe", O_RDONLY, 0);
-	if (trace_fd < 0)
-		return;
+    trace_fd = open(DEBUGFS "trace_pipe", O_RDONLY, 0);
+    if (trace_fd < 0)
+        return;
 
-	while (1) {
-		static char buf[4096];
-		ssize_t sz;
+    while (1) {
+        static char buf[4096];
+        ssize_t sz;
 
-		sz = read(trace_fd, buf, sizeof(buf));
-		if (sz > 0) {
-			buf[sz] = 0;
-			puts(buf);
-		}
-	}
-
+        sz = read(trace_fd, buf, sizeof(buf));
+        if (sz > 0) {
+            buf[sz] = 0;
+            puts(buf);
+        }
+    }
     
     close(trace_fd);
 }
 
 int main(int argc, char **argv)
 {
-	int logFlag = 0;
-	int error = 0;
-	char *cg_path;
-	char fn[500];
-	char *prog;
-	int cg_fd;
-	struct bpf_object *obj;
-	int prog_fd;
-	////
+    int logFlag = 0;
+    int error = 0;
+    char *cg_path;
+    char fn[500];
+    char *prog;
+    int cg_fd;
+    struct bpf_object *obj;
+    int prog_fd;
 
-	
-	if (!strcmp(argv[1], "-l"))
-	{
-		logFlag = 1;
-		////
-	}
-
-	prog = argv[argc - 1];
-	cg_path = argv[argc - 2];
-	if (strlen(prog) > 480)
-	{
-		fprintf(stderr, "ERROR: program name too long (> 480 chars)\n");
-		return 3;
-	}
-	cg_fd = open(cg_path, O_DIRECTORY, O_RDONLY);
-
-	if (!strcmp(prog + strlen(prog) - 2, ".o"))
-		strcpy(fn, prog);
-	else
-		sprintf(fn, "%s_kern.o", prog);
-	if (logFlag)
-		printf("loading bpf file:%s\n", fn);
-	
-
-	obj = bpf_object__open_file(fn, NULL);
-	if (!obj) {
-	    printf("ERROR: bpf_object__open_file failed for: %s\n", fn);
-	    return 4;
-	}
-
-	if (bpf_object__load(obj)) {
-	    printf("ERROR: bpf_object__load failed\n");
-	    bpf_object__close(obj);
-	    return 4;
-	}
-
-	struct bpf_program *bpf_prog = bpf_object__find_program_by_name(obj, "bpf_basertt");
-    	if (!bpf_prog) {
-            printf("ERROR: bpf_object__find_program_by_name failed\n");
-            bpf_object__close(obj);
-            return 4;
+    if (argc < 3) {
+        fprintf(stderr, "Usage: %s [-l] <cgroup_path> <program>\n", argv[0]);
+        return 1;
+    }
+    
+    if (!strcmp(argv[1], "-l")) {
+        logFlag = 1;
+        if (argc < 4) {
+            fprintf(stderr, "Usage with logging: %s -l <cgroup_path> <program>\n", argv[0]);
+            return 1;
         }
+    }
 
-	prog_fd = bpf_program__fd(bpf_prog);
-	if (logFlag)
-		printf("TCP BPF Loaded %s\n", fn);
-
-	struct bpf_map *key_cong_map = bpf_object__find_map_by_name(obj, "key_cong_map");
-
+    prog = argv[argc - 1];
+    cg_path = argv[argc - 2];
     
-    	if (!key_cong_map) {
-      	  printf("ERROR: could not find required maps\n");
-       	  bpf_object__close(obj);
-       	  return 4;
-   	}
+    if (strlen(prog) > 480) {
+        fprintf(stderr, "ERROR: program name too long (> 480 chars)\n");
+        return 3;
+    }
     
-   	int key_cong_map_fd = bpf_map__fd(key_cong_map);
+    cg_fd = open(cg_path, O_DIRECTORY, O_RDONLY);
+    if (cg_fd < 0) {
+        fprintf(stderr, "ERROR: failed to open cgroup path: %s\n", strerror(errno));
+        return 2;
+    }
 
+    if (!strcmp(prog + strlen(prog) - 2, ".o"))
+        strcpy(fn, prog);
+    else
+        sprintf(fn, "%s_kern.o", prog);
+        
+    if (logFlag)
+        printf("loading bpf file: %s\n", fn);
     
-    	// Attacher le programme
-    	error = bpf_prog_attach(prog_fd, cg_fd, BPF_CGROUP_SOCK_OPS, 0);
-    	if (error) {
-        	printf("ERROR: bpf_prog_attach: %d (%s)\n",
-               	       error, strerror(errno));
-                bpf_object__close(obj);
-                return 5;
+    obj = bpf_object__open_file(fn, NULL);
+    if (!obj) {
+        printf("ERROR: bpf_object__open_file failed for: %s\n", fn);
+        return 4;
+    }
+
+    if (bpf_object__load(obj)) {
+        printf("ERROR: bpf_object__load failed\n");
+        bpf_object__close(obj);
+        return 4;
+    }
+
+    struct bpf_program *bpf_prog = bpf_object__find_program_by_name(obj, "bpf_basertt");
+    if (!bpf_prog) {
+        printf("ERROR: bpf_object__find_program_by_name failed\n");
+        bpf_object__close(obj);
+        return 4;
+    }
+
+    prog_fd = bpf_program__fd(bpf_prog);
+    if (logFlag)
+        printf("TCP BPF Loaded %s\n", fn);
+
+    struct bpf_map *key_cong_map = bpf_object__find_map_by_name(obj, "key_cong_map");
+    if (!key_cong_map) {
+        printf("ERROR: could not find required maps\n");
+        bpf_object__close(obj);
+        return 4;
+    }
+    
+    int key_cong_map_fd = bpf_map__fd(key_cong_map);
+    
+    init_map(key_cong_map_fd);
+    
+    error = bpf_prog_attach(prog_fd, cg_fd, BPF_CGROUP_SOCK_OPS, 0);
+    if (error) {
+        printf("ERROR: bpf_prog_attach: %d (%s)\n",
+               error, strerror(errno));
+        bpf_object__close(obj);
+        return 5;
+    }
+    else if (logFlag) {
+        int ret_from_fork;
+        if ((ret_from_fork = fork()) == -1) {
+            perror("fork");
+            exit(EXIT_FAILURE);
         }
-	else if (logFlag)
-	{
-		int ret_from_fork;
-		if ((ret_from_fork = fork()) == -1)
-		{
-			perror("fork");
-			exit(EXIT_FAILURE);
-		}
-		////
-		else
-		{
-			printf("enter read trace pip\n");
-			read_trace_pipe();
-			bpf_object__close(obj);
-			return 0;
-
-		}
-	}
-	
-	bpf_object__close(obj);
-	return error;
+        else if (ret_from_fork == 0) {
+            printf("enter read trace pipe\n");
+            read_trace_pipe();
+            exit(0);
+        }
+    }
+    
+    printf("Program attached successfully. Press Ctrl+C to detach and exit.\n");
+    
+    while (1) {
+        sleep(1);
+    }
+    
+    bpf_object__close(obj);
+    return error;
 }
--- tcp_changecc_kern.c
+++ tcp_changecc_kern.c
@@ -1,4 +1,3 @@
-
 #include <uapi/linux/bpf.h>
 #include <uapi/linux/tcp.h>
 #include <uapi/linux/if_ether.h>
@@ -10,16 +9,13 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_endian.h>
 
-
-
-
 #define DEBUG 1
 
 struct connection_tuple {
-    long src_ip;    
-    long dst_ip;    
-    long src_port;    
-    long dst_port;   	
+    __u32 src_ip;    
+    __u32 dst_ip;    
+    __u16 src_port;  
+    __u16 dst_port;   
 };
 
 
@@ -28,225 +24,58 @@ struct {
     __uint(max_entries, 20);
     __uint(map_flags, BPF_F_NO_PREALLOC);
     __type(key, struct connection_tuple);
-    __type(value, char[10]);
+    __type(value, char[16]);  
 } key_cong_map SEC(".maps");
 
-struct tcp_cc_names {
-    char reno[5];       
-    char cubic[6];      
-    char illinois[9];   
-    char vegas[6];      
-    char bbr[4];        
-    char westwood[9];   
-    char highspeed[10]; 
-    char hybla[6];      
-    char scalable[9];   
-    char yeah[5];       
-    char dctcp[6];      
-} __attribute__((packed));
-
-
-const struct tcp_cc_names cc_names SEC(".rodata") = {
-    .reno = "reno",
-    .cubic = "cubic",
-    .illinois = "illinois",
-    .vegas = "vegas",
-    .bbr = "bbr",
-    .westwood = "westwood",
-    .highspeed = "highspeed",
-    .hybla = "hybla",
-    .scalable = "scalable",
-    .yeah = "yeah",
-    .dctcp = "dctcp"
-};
-
-static inline void init_map()
-{
-
-    long common_ip = 0x7F000001;  
-    long common_src_port = 5001;   
-    
-
-    struct connection_tuple tuple0;
-    tuple0.src_ip = common_ip;
-    tuple0.dst_ip = common_ip;
-    tuple0.src_port = common_src_port;
-    tuple0.dst_port = 0;      
-    
-
-    bpf_map_update_elem(&key_cong_map, &tuple0, cc_names.reno, BPF_ANY);
-
-
-    struct connection_tuple tuple1;
-
-    tuple1.src_ip = common_ip;
-    tuple1.dst_ip = common_ip;
-    tuple1.src_port = common_src_port;
-    tuple1.dst_port = 5004;   
-    
-
-    bpf_map_update_elem(&key_cong_map, &tuple1, cc_names.cubic, BPF_ANY);
-
-
-    struct connection_tuple tuple2;
-
-    tuple2.src_ip = common_ip;
-    tuple2.dst_ip = common_ip;
-    tuple2.src_port = common_src_port;
-    tuple2.dst_port = 5005;   
-    
-
-    bpf_map_update_elem(&key_cong_map, &tuple2, cc_names.illinois, BPF_ANY);
-
-
-    struct connection_tuple tuple3;
-
-    tuple3.src_ip = common_ip;
-    tuple3.dst_ip = common_ip;
-    tuple3.src_port = common_src_port;
-    tuple3.dst_port = 5006;   
-    
-
-    bpf_map_update_elem(&key_cong_map, &tuple3, cc_names.vegas, BPF_ANY);
-
-
-    struct connection_tuple tuple4;
-
-    tuple4.src_ip = common_ip;
-    tuple4.dst_ip = common_ip;
-    tuple4.src_port = common_src_port;
-    tuple4.dst_port = 5007;   
-    
-
-    bpf_map_update_elem(&key_cong_map, &tuple4, cc_names.bbr, BPF_ANY);
-
-
-    struct connection_tuple tuple5;
-
-    tuple5.src_ip = common_ip;
-    tuple5.dst_ip = common_ip;
-    tuple5.src_port = common_src_port;
-    tuple5.dst_port = 5008;   
-    
-
-    bpf_map_update_elem(&key_cong_map, &tuple5, cc_names.westwood, BPF_ANY);
-
-
-    struct connection_tuple tuple6;
-
-    tuple6.src_ip = common_ip;
-    tuple6.dst_ip = common_ip;
-    tuple6.src_port = common_src_port;
-    tuple6.dst_port = 5009;   
-    
-
-    bpf_map_update_elem(&key_cong_map, &tuple6, cc_names.highspeed, BPF_ANY);
-
-
-    struct connection_tuple tuple7;
-
-    tuple7.src_ip = common_ip;
-    tuple7.dst_ip = common_ip;
-    tuple7.src_port = common_src_port;
-    tuple7.dst_port = 5010;   
-    
-
-    bpf_map_update_elem(&key_cong_map, &tuple7, cc_names.hybla, BPF_ANY);
-
-
-    struct connection_tuple tuple8;
-
-    tuple8.src_ip = common_ip;
-    tuple8.dst_ip = common_ip;
-    tuple8.src_port = common_src_port;
-    tuple8.dst_port = 5011;   
-    
-
-    bpf_map_update_elem(&key_cong_map, &tuple8, cc_names.scalable, BPF_ANY);
-
-
-    struct connection_tuple tuple9;
-
-    tuple9.src_ip = common_ip;
-    tuple9.dst_ip = common_ip;
-    tuple9.src_port = common_src_port;
-    tuple9.dst_port = 5012;   
-    
-
-    bpf_map_update_elem(&key_cong_map, &tuple9, cc_names.yeah, BPF_ANY);
-
-
-    struct connection_tuple tuple10;
-
-    tuple10.src_ip = common_ip;
-    tuple10.dst_ip = common_ip;
-    tuple10.src_port = common_src_port;
-    tuple10.dst_port = 5013;   
-    
-
-    bpf_map_update_elem(&key_cong_map, &tuple10, cc_names.dctcp, BPF_ANY);
-}
 SEC("sockops")
 int bpf_basertt(struct bpf_sock_ops *skops)
 {
-	init_map();
-	int op = (int)skops->op;
-	long dport = (long)bpf_ntohl(skops->remote_port);
-	long lport = (long)skops->local_port;
-	long nlip = (long)bpf_ntohl(skops->local_ip4);
-	long ndip = (long)bpf_ntohl(skops->remote_ip4);
-	bpf_printk("dport :%ld lport:%ld\n", dport, lport);
-	bpf_printk("nlip :%ld ndip:%ld\n", nlip, ndip);
-	switch (op)
-	{
-	case BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB:
-	//case BPF_SOCK_OPS_ACTIVE_ESTABLISHED_CB:
-	//case BPF_SOCK_OPS_TCP_ACK_CB:
-		bpf_printk("enter BPF_SOCK_OPS_TCP_ACK_CB\n");
+    int op = (int)skops->op;
+    __u32 dport = (__u32)bpf_ntohl(skops->remote_port);
+    __u16 lport = (__u16)skops->local_port;
+    __u32 nlip = (__u32)bpf_ntohl(skops->local_ip4);
+    __u32 ndip = (__u32)bpf_ntohl(skops->remote_ip4);
+    
+    bpf_printk("dport :%u lport:%u\n", dport, lport);
+    bpf_printk("nlip :%u ndip:%u\n", nlip, ndip);
+    
+    switch (op)
+    {
+    case BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB:
+    //case BPF_SOCK_OPS_ACTIVE_ESTABLISHED_CB:
+    //case BPF_SOCK_OPS_TCP_ACK_CB:
+        bpf_printk("enter BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB\n");
 
-		//long cc_id = dport;
-		struct connection_tuple cc_id;
-	    
+        struct connection_tuple cc_id;
+        cc_id.src_ip = nlip;     
+        cc_id.dst_ip = ndip;    
+        cc_id.src_port = lport;           
+        cc_id.dst_port = dport;
+        
+        bpf_printk("cc_id: src_ip=%x dst_ip=%x", cc_id.src_ip, cc_id.dst_ip);
+        bpf_printk("cc_id: src_port=%u dst_port=%u", cc_id.src_port, cc_id.dst_port);
+        
+        char *con_str = bpf_map_lookup_elem(&key_cong_map, &cc_id);
+        bpf_printk("constr: %s\n", con_str);
+        
+        char cong[20];
+        bpf_getsockopt(skops, SOL_TCP, TCP_CONGESTION, cong, sizeof(cong));
+        bpf_printk("before cc:%s\n", cong);
 
-
-	        cc_id.src_ip = nlip;     
-	        cc_id.dst_ip = ndip;    
-	    
-
-	        cc_id.src_port = lport;           
-	        cc_id.dst_port = dport;
-	        bpf_printk("cc_id: src_ip=%x dst_ip=%x", cc_id.src_ip, cc_id.dst_ip);
-
-		bpf_printk("cc_id: src_port=%u dst_port=%u", cc_id.src_port, cc_id.dst_port);
-		char *con_str = bpf_map_lookup_elem(&key_cong_map, &cc_id);
-		bpf_printk("constr: %s\n", con_str);
-		char cong[20];
-		bpf_getsockopt(skops, SOL_TCP, TCP_CONGESTION,
-					   cong, sizeof(cong));
-		bpf_printk("before cc:%s\n", cong);
-
-		if (con_str == NULL)
-		{
-			return 1;
-		}
-		
-		bpf_setsockopt(skops, SOL_TCP, TCP_CONGESTION, con_str, 10);
-		bpf_getsockopt(skops, SOL_TCP, TCP_CONGESTION, cong, sizeof(cong));
-		//int r = bpf_map_delete_elem(&cong_map, &cc_id);
-		//if (r == 0)
-			//bpf_printk("Element deleted from the map\n");
-		//else
-		//	bpf_printk("Failed to delete element from the map: %d\n", r);
-		//break;
-
-		bpf_printk("after cc:%s\n", cong);
-
-		break;
-	
-	}
-	char nv[] = "nv";
-	int rv = 0, n;
-	skops->reply = rv;
-	return 1;
+        if (con_str == NULL)
+        {
+            return 1;
+        }
+        
+        bpf_setsockopt(skops, SOL_TCP, TCP_CONGESTION, con_str, 16);
+        bpf_getsockopt(skops, SOL_TCP, TCP_CONGESTION, cong, sizeof(cong));
+        bpf_printk("after cc:%s\n", cong);
+        break;
+    }
+    
+    int rv = 0;
+    skops->reply = rv;
+    return 1;
 }
+
 char _license[] SEC("license") = "GPL";
