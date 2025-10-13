a15fde2ea0d6ef5c8c2638eb6fb1d8c6eb5f84fb
sofianealhoz <sofiane.alhoz@gmail.com>
2025-10-13 17:55:05 +0200

    django backend and vue page init

--- web/benchmarks/views.py
+++ web/benchmarks/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+def resultats_list(request):
+    resultats = [1,3,5]
+    return JsonResponse({"resultats": resultats})
--- web/config/settings.py
+++ web/config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'benchmarks.apps.BenchmarksConfig'
+    'benchmarks.apps.BenchmarksConfig',
 ]
 
 MIDDLEWARE = [
